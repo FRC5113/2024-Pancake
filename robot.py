a837e062ae5206eac16a27b42ed0cc664dfa6985
Ryan Radtke <110181914+Ryanforce08@users.noreply.github.com>
2024-04-18 16:58:48 -0700

    adds ids and now uses xbox controller cause why not

--- robot.py
+++ robot.py
@@ -16,21 +16,18 @@ class MyRobot(magicbot.MagicRobot):
     # Creates objects as the definition implies
     def createObjects(self):
         # Motor controllers for the drivetrain
-        """remeber to change the ids
-            only 2 motors cause pancake has 2 at the time of writing"""
+        """only 2 motors cause pancake has 2 at the time of writing"""
         #self.frontleftmotor = phoenix5.WPI_TalonSRX(15)
-        self.frontrightmotor = phoenix5.WPI_TalonSRX(18)
-        self.backleftmotor = phoenix5.WPI_TalonSRX(55)
+        self.frontrightmotor = phoenix5.WPI_TalonSRX(17)
+        self.backleftmotor = phoenix5.WPI_TalonSRX(22)
         #self.backrightmotor = phoenix5.WPI_TalonSRX(12)
 
         # Joystick for user input
-        self.joy = wpilib.Joystick(0)
+        self.joy = wpilib.XboxController(0)
 
         # Gyroscope sensor for orientation tracking
         self.navx = navx.AHRS.create_spi()
 
-        # Throttle value from joystick for controlling speed
-        self.speed = self.joy.getThrottle()
 
     @property
     def get_angle(self):
@@ -41,13 +38,9 @@ class MyRobot(magicbot.MagicRobot):
 
         # Drive the robot using arcade drive with adjusted speed
         self.drivetrain.arcadeDrive(
-            -self.joy.getX() * 0.5,
-            self.joy.getY() * 0.5
+            -self.joy.getLeftX() * 0.5,
+            self.joy.getRightY() * 0.5
         )
-
-        # If trigger on joystick is pressed, command the robot to turn to a specific angle
-        if self.joy.getTrigger():
-            self.drive_control.turn_to_angle(180)
 
         # Update SmartDashboard with joystick values and current gyroscope angle
         SmartDashboard.putNumber("Joystick X value", self.joy.getY())
